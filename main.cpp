6c83822ece0852e1e2f65771aadc37f2c1b41365
Cristian-Nastase <cristianemail@duck.com>
2026-03-16 17:09:38 +0200

    Implemented default functions with helpers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
+#include <cstring>
 #include <iostream>
-
-using namespace std;
 
 class Selector {
     private:
@@ -9,13 +8,121 @@ class Selector {
         char* selectorString;
         bool cascades;
         bool compoundSelector;
+        int* returnNewSpecificity();
+        int* calculateSpecificity(const char *selectorString);
+        bool checkCompound(const char *selectorString);
     public:
         Selector();
         Selector(char* selectorString, bool cascades);
-        Selector(const Selector &selector);
-        Selector& operator=(const Selector &selector);
+        Selector(const Selector &obj);
+        Selector& operator=(const Selector &obj);
         ~Selector();
 };
+
+// 0. id 1. class 2. element
+int* returnNewSpecificity() {
+    int* specificity = new int[3];
+    for (int i = 0; i < 3; i++) {
+        specificity[i] = 0;
+    }
+
+    return specificity;
+}
+
+bool checkCompound(const char *selectorString) {
+    char *combinators = strcpy(new char[7], ":> ~+|");
+
+    for (int i = 0; i < strlen(combinators); i++) {
+        if (strchr(selectorString, combinators[i]) != nullptr) {
+            delete[] combinators;
+            combinators = nullptr;
+            return true;
+        }
+    }
+
+    delete[] combinators;
+    combinators = nullptr;
+
+    return false;
+}
+
+int* calculateSpecificity(const char *selectorString) {
+    int* specificity = returnNewSpecificity();
+
+    bool lastCharSpace;
+    for (int i = 0; i < strlen(selectorString); i++) {
+        switch (selectorString[i]) {
+            case '#':
+                specificity[0]++;
+                break;
+            case '.':
+                specificity[1]++;
+                break;
+            case ' ':
+                lastCharSpace = true;
+                break;
+            default:
+                if (!lastCharSpace)
+                    break;
+
+                specificity[2]++;
+                lastCharSpace = false;
+        }
+    }
+
+    return specificity;
+}
+
+Selector::Selector() {
+    blocksNumber++;
+    selectorString = strcpy(new char[2], "*");
+    cascades = true;
+    compoundSelector = false;
+    specificity = returnNewSpecificity();
+}
+
+Selector::Selector(char* selectorString, bool cascades) {
+    blocksNumber++;
+    this->selectorString = strcpy(new char[strlen(selectorString) + 1], selectorString);
+    this-> cascades = cascades;
+    specificity = calculateSpecificity(selectorString);
+    compoundSelector = checkCompound(selectorString);
+}
+
+Selector::Selector(const Selector &obj) {
+    blocksNumber++;
+    selectorString = strcpy(new char[strlen(obj.selectorString) + 1], obj.selectorString);
+    cascades = obj.cascades;
+    compoundSelector = obj.compoundSelector;
+    specificity = new int[3];
+    for (int i = 0; i < 3; i++) {
+        specificity[i] = obj.specificity[i];
+    }
+}
+
+Selector& Selector::operator=(const Selector &obj) {
+    if (this == &obj)
+        return *this;
+
+    delete[] selectorString;
+    selectorString = strcpy(new char[strlen(obj.selectorString) + 1], selectorString);
+
+    cascades = obj.cascades;
+    compoundSelector = obj.compoundSelector;
+
+    delete[] specificity;
+    specificity = new int[3];
+    for (int i = 0; i < 3; i++) {
+        specificity[i] = obj.specificity[i];
+    }
+
+    return *this;
+}
+
+Selector::~Selector() {
+    delete[] specificity;
+    delete[] selectorString;
+}
 
 class Property {
     private:
@@ -68,5 +175,4 @@ class Function {
 };
 
 int main() {
-
 }
